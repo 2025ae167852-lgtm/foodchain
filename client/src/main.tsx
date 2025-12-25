ad46fdb208aaa64bd7a1c3373c6bad014d76d54e
kinyuagk <kinyuakinyingi@gmail.com>
2025-12-25 09:46:28 +0300

    Rebuild for correct base path and deployment

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { Router } from "wouter"; from "react-router-dom";
+import App from "./App";p";
+import "./index.css";import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(createRoot(document.getElementById("root")!).render(
+
+
+
+
+
+);  </Router>    <App />  <Router base="/foodchain">  <BrowserRouter basename="/foodchain/">
+    <App />
+  </BrowserRouter>
+);
